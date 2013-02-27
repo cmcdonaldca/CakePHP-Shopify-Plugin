31e07d8008a58d6936021b752ce2fbe0bcfd95bc
Colin McDonald <cmcdonaldca@gmail.com>
2013-02-26 23:00:33 -0500

    Added info on setting the scope

--- Config/shopify.php
+++ Config/shopify.php
@@ -3,6 +3,7 @@
 $config = array(
                 'api_key' => '',
                 'shared_secret' => '',
+                'scope' => 'write_content,write_products', // <-- change to the scope you need
                 'is_private_app' => false
         );
 ?>
